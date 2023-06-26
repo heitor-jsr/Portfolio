1beaa5836852102f3f3d8abba6cd5c02df31137c
heitor-jsr <heitorscalon@gmail.com>
2023-06-26 19:58:38 -0300

    ads

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://github.com/heitor-jsr/Portfolio/blob/master/src/Assets/Curriculo-1.pdf";
+  "https://raw.githubusercontent.com/heitor-jsr/Portfolio/master/src/Assets/Curriculo-1.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
